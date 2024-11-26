15d61ed91fd2cbf44cc6ccde1d0cb016c9afbfb6
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-26 10:10:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
-# パッケージ情報
 setup(
-    name='NekomiAI',  # パッケージ名
-    version='0.1',  # バージョン
-    packages=find_packages(),  # パッケージの自動探索
+    name='Nekomi-AI',
+    version='0.1',
+    packages=find_packages(),
     install_requires=[],
-    description='NEKO HA SEKAI WO KAERU NODA.',  # ライブラリの簡単な説明
-    author='MUNETORI COREWORLD',  # 作者名
-    author_email='your.email@example.com',  # 作者のメールアドレス
-    url='https://github.com/yourusername/Coreworld',  # GitHub のリポジトリ URL
-    classifiers=[  # PyPIにアップロードするときの分類
+    description='NEKOMI HA SEKAI WO SUKUU NODA.',
+    author='MUNETORI COREWORLD',
+    url='https://github.com/coreworldyoutube/Nekomi-AI',
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # 必要なPythonのバージョン
+    python_requires='>=3.6',
 )
