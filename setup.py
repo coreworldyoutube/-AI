f7f2880f1ac88faa940dd08e9f338e9660d3b518
Core World <159610152+coreworldyoutube@users.noreply.github.com>
2024-11-26 10:27:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Nekomi-AI',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[],
     description='NEKOMI HA SEKAI WO SUKUU NODA.',
